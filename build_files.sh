d371d59f9c82fcdad20a527771f061ee481a7eba
weii1501 <vodacluc15@gmail.com>
2023-08-18 16:03:36 +0700

    sssggg

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,7 @@
+
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+pip install gunicorn
 pip install rpds-py
 pip install -r requirementsVercel.txt
+
+python manage.py collectstatic --no-input
