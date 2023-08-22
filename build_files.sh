7a7cfb08af9931239655be00eab6e99a4f9967b2
weii1501 <vodacluc15@gmail.com>
2023-08-22 14:58:59 +0700

    uu

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,9 @@
 python3 -m venv /opt/render/project/src/venv
-source /opt/render/project/src/venv/bin/activate
 
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 pip install gunicorn
 pip install rpds-py
 pip install -r requirementsVercel.txt
 
+source /opt/render/project/src/venv/bin/activate
 python manage.py collectstatic --noinput
