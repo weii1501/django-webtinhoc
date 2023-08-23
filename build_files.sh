340cf21bf039bb4083a73cf159c8460194daa2c5
weii1501 <vodacluc15@gmail.com>
2023-08-23 13:56:26 +0700

    ggg

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,9 @@ python3 -m venv /opt/render/project/src/.venv
 source /opt/render/project/src/.venv/bin/activate
 
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade setuptools
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade wheel
+pip install django-allauth
 pip install gunicorn
 pip install rpds-py
 pip install -r requirementsVercel.txt
