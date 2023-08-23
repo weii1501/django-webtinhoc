784f11193fbb6f2d9b1324903aead2a920ae389a
weii1501 <vodacluc15@gmail.com>
2023-08-23 15:11:53 +0700

    pip install django

--- build_files.sh
+++ build_files.sh
@@ -2,13 +2,12 @@ python3 -m venv /opt/render/project/src/.venv
 source /opt/render/project/src/.venv/bin/activate
 
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
-/opt/render/project/src/.venv/bin/python -m pip install --upgrade setuptools
-/opt/render/project/src/.venv/bin/python -m pip install --upgrade wheel
-pip install daphne
-pip install django-allauth
-pip install gunicorn
-pip install rpds-py
-pip install -r requirementsVercel.txt
+pip install django
+#pip install daphne
+#pip install django-allauth
+#pip install gunicorn
+#pip install rpds-py
+#pip install -r requirementsVercel.txt
 
 
 python manage.py collectstatic --noinput
